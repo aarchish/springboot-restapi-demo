6f2aaf0b7497f3252a9f77f9311ff78c7342666e
Aarchish Jindal <aarchish0@gmail.com>
2024-12-23 20:39:34 +0530

    updated file name

---
+++ src/main/java/com/jstech/springboot/rest_api_demo/survey/Question.java
@@ -0,0 +1,44 @@
+package com.jstech.springboot.rest_api_demo.survey;
+
+import java.util.List;
+
+public class Question {
+    private String id;
+    private String description;
+    private List<String> option;
+    private String correctAnswer;
+
+    public Question(String id, String description, List<String> option, String correctAnswer) {
+        super();
+        this.id = id;
+        this.description = description;
+        this.option = option;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getOption() {
+        return option;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "Survey{" +
+                "id='" + id + '\'' +
+                ", description='" + description + '\'' +
+                ", option=" + option +
+                ", correctAnswer='" + correctAnswer + '\'' +
+                '}';
+    }
+}
