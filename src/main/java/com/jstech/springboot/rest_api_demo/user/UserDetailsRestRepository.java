4d656de5974dddc7f8aa78536c2b04ebcab86f7e
Aarchish Jindal <aarchish0@gmail.com>
2024-12-26 20:11:40 +0530

    corrected JpaRepository

--- src/main/java/com/jstech/springboot/rest_api_demo/user/UserDetailsRestRepository.java
+++ src/main/java/com/jstech/springboot/rest_api_demo/user/UserDetailsRestRepository.java
@@ -1,11 +1,9 @@
 package com.jstech.springboot.rest_api_demo.user;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@RepositoryRestResource
-public interface UserDetailsRestRepository extends PagingAndSortingRepository<UserDetails, Long> {
+public interface UserDetailsRestRepository extends JpaRepository<UserDetails, Long> {
     List<UserDetails> findByRole(String role);
 }
